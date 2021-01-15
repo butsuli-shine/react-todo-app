be34c2d1b5d8986ac66bec7bedf36d00128846c6
butsuli-shine <luluchey.1223@gmail.com>
2021-01-15 11:32:51 +0900

    add: skills svgアイコンを追加

--- frontend/src/conponents/Home/Home.jsx
+++ frontend/src/conponents/Home/Home.jsx
@@ -1,17 +1,49 @@
 const Home = () => {
+  const progress = {
+    25:{
+      width: 700/4
+    },
+    50:{
+      width: 700/2
+    },
+    75:{
+      width: 700/2 + 700/4
+    },
+    100:{
+      width: 700
+    }
+  }
+
   return (
     <div className="Home">
-      <h2> Introduction </h2>
+      <h3> <i data-vi="doc"></i>Introduction </h3>
       <blockquote className="blockquote">
-        <h3>Name</h3>
-        <p>Sakamoto Keisuke</p>
+        <h4><i data-vi="user"></i> Name</h4>
+        <p><i data-vi="arrow-right"></i>Sakamoto Keisuke</p>
       </blockquote>
       <blockquote className="blockquote">
-        <h3>Age</h3>
-        <p>18</p>
+        <h4><i data-vi="heart"></i>Age</h4>
+        <p><i data-vi="arrow-right"></i>18</p>
       </blockquote>
-      <h3>skills</h3>
-      <p>a</p>
+      <blockquote>
+        <h4> <i data-vi="graduation-cap"></i>School</h4>
+        <p><i data-vi="arrow-right"></i>National Institute of Technology, Gunma College</p>
+      </blockquote>
+      <blockquote>
+        <h4><i data-vi="graph-poly"></i>skills</h4>
+        <img src="./img/html5.svg" alt="" width = "20"/>html5
+        <div class="progress">
+          <div class="progress-bar bg-success" role="progressbar" style={progress[50]} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <img src="./img/css3.svg" alt="" width="20"/>css3/scss
+        <div class="progress">
+          <div class="progress-bar bg-success" role="progressbar" style={progress[75]} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <img src="./img/javascript.svg" alt="" width="20"/>JavaScript/ <img src="./img/typescript.svg" alt="" width="20"/> Typescript
+        <div class="progress">
+          <div class="progress-bar bg-success" role="progressbar" style={progress[50]} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        </blockquote>
     </div>
   );
 };
